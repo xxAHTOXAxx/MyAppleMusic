70b7a956cd2ec5088defe2ec1f7bf4b2e9d6256c
xxAHTOXAxx <133795951+xxAHTOXAxx@users.noreply.github.com>
2023-11-28 22:11:10 +0400

    Initial Commit

---
+++ MyAppleMusic/MyAppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppleMusicApp.swift
+//  MyAppleMusic
+//
+//  Created by Антон Исаков on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
