21ffff79d325d6a46441bdef3fdb44f4a748a2ad
xxAHTOXAxx <133795951+xxAHTOXAxx@users.noreply.github.com>
2023-12-16 22:58:45 +0400

    CompletedSearch

--- MyAppleMusic/View/SearchView/LazyVGridViewSearch.swift
+++ MyAppleMusic/View/SearchView/LazyVGridViewSearch.swift
@@ -8,6 +8,8 @@ struct LazyVGridViewSearch: View {
     @State private var items = ModelItemsForSearch.itemsArray
     @State private var isTapped = false
     @State private var isEditing = false
+    @State private var isSongListVisible = false
+    private var array = ["Песня 1", "Песня 2", "Песня 3", "Песня 4"]
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -23,7 +25,7 @@ struct LazyVGridViewSearch: View {
                                 .foregroundColor(.gray)
                                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                                 .padding(.leading, 8)
-                     
+                            
                             if isEditing {
                                 Button(action: {
                                     self.searchText = ""
@@ -31,18 +33,22 @@ struct LazyVGridViewSearch: View {
                                     Image(systemName: "multiply.circle.fill")
                                         .foregroundColor(.gray)
                                         .padding(.trailing, 8)
+
                                 }
                             }
                         }
                     )
                     .onTapGesture {
                         self.isEditing = true
+                        self.isSongListVisible = true
                     }
+                
                 if isEditing {
                     Button(action: {
                         self.isEditing = false
                         self.searchText = ""
-
+                        self.isSongListVisible = false
+                        
                     }) {
                         Text("Отменить")
                     }
@@ -50,42 +56,58 @@ struct LazyVGridViewSearch: View {
                     .transition(.move(edge: .trailing))
                     .animation(.default)
                 }
+                
             }
-        }
-        .padding()
-            Text(title)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.leading, -65)
+            .padding()
+            if !isEditing {
+                Text(title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.leading, 10)
+            }
             
-            ScrollView(.vertical, showsIndicators: false) {
-                LazyVGrid(columns: columns, alignment: .center) {
-                    ForEach (items, id: \.self) { item in
-                        NavigationLink {
-                            SearchViewItem(genreOfMusic: item.genreOfMusic)
-                        } label: {
-                            Image (item.itemImage)
-                                .resizable()
-                                .cornerRadius (10)
-                                .frame (height: 125)
-                                .overlay(alignment: .bottomLeading) {
-                                    Text(item.genreOfMusic)
-                                        .multilineTextAlignment(
-                                            .leading)
-                                        .font(.system(size: 13,
-                                                      weight: .bold))
-                                        .foregroundColor(.white)
-                                        .padding([.horizontal,
-                                                  .bottom])
-                                }
+            if isSongListVisible {
+                List {
+                    ForEach(array.filter { $0.hasPrefix(searchText) || searchText.isEmpty }, id: \.self) { searchText in
+                        Text(searchText)
+                            .onTapGesture {
+                                print("Выбрана песня: \(searchText)")
+                                self.isSongListVisible = false
+                            }
+                    }
+                }
+                .navigationBarTitle(Text("Песни"))
+            } else {
+                ScrollView(.vertical, showsIndicators: false) {
+                    LazyVGrid(columns: columns, alignment: .center) {
+                        ForEach (items, id: \.self) { item in
+                            NavigationLink {
+                                SearchViewItem(genreOfMusic: item.genreOfMusic)
+                            } label: {
+                                Image (item.itemImage)
+                                    .resizable()
+                                    .cornerRadius (10)
+                                    .frame (height: 125)
+                                    .overlay(alignment: .bottomLeading) {
+                                        Text(item.genreOfMusic)
+                                            .multilineTextAlignment(
+                                                .leading)
+                                            .font(.system(size: 13,
+                                                          weight: .bold))
+                                            .foregroundColor(.white)
+                                            .padding([.horizontal,
+                                                      .bottom])
+                                    }
+                            }
                         }
                     }
                 }
+                .padding(.horizontal, 10)
             }
-            .padding(.horizontal, 10)
         }
     }
-
+}
 #Preview {
     LazyVGridViewSearch()
 }
+
